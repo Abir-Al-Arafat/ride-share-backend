687e38cbc2c930575e02c8948d38744a886575d0
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-04-30 17:16:59 +0600

    interface: user

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,15 @@
+import { Model, ObjectId } from "mongoose";
+
+export interface IUser {
+  _id?: ObjectId;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  dob?: Date; //optional
+  role?: string;
+}
+
+export interface UserRequest extends Request {
+  user: IUser;
+}
