43c7fbb5f0078d006cc8a5f8ab0e968723263445
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-05-26 12:35:06 +0600

    update: user data interface optional

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -2,14 +2,14 @@ import { Model, ObjectId } from "mongoose";
 
 export interface IUser {
   _id?: ObjectId;
-  name: string;
-  username: string;
-  email: string;
+  name?: string;
+  username?: string;
+  email?: string;
   password?: string;
   dob?: Date; //optional
   role?: string;
 }
 
 export interface UserRequest extends Request {
-  user: IUser;
+  user?: IUser;
 }
