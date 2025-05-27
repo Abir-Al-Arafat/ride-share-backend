6ca8996b85a01174398da05befb8dab92241a214
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-05-27 18:08:46 +0600

    add: vehicle model

---
+++ src/models/vehicle.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, Document, ObjectId } from "mongoose";
+
+export interface IVehicle extends Document {
+  user: ObjectId; // Reference to the User
+  carModel: string;
+  manufactureYear: number;
+  vin: string;
+  images: string[]; // Array of image URLs or paths
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const VehicleSchema = new Schema<IVehicle>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    carModel: { type: String, required: true },
+    manufactureYear: { type: Number, required: true },
+    vin: { type: String, required: true, unique: true },
+    images: [{ type: String }], // Store image URLs or file paths
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IVehicle>("Vehicle", VehicleSchema);
