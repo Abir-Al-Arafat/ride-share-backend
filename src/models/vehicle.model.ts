278624c5517d99f368734e4dad5f7dd471e792f3
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-05-28 15:18:00 +0600

    update: vin uniqueness removed

--- src/models/vehicle.model.ts
+++ src/models/vehicle.model.ts
@@ -15,7 +15,11 @@ const VehicleSchema = new Schema<IVehicle>(
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     carModel: { type: String, required: true },
     manufactureYear: { type: Number, required: true },
-    vin: { type: String, required: true, unique: true },
+    vin: {
+      type: String,
+      required: true,
+      // unique: true
+    },
     images: [{ type: String }], // Store image URLs or file paths
   },
   { timestamps: true }
